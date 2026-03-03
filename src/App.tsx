afb3ae88c2fbe52a2614467ed5c5ec560b35203d
rajeswar-hash <rajeswarbind39@gmail.com>
2026-03-03 09:11:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/tactic9-mastermind/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
